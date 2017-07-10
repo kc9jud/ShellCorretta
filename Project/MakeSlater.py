e3a9780996fd67987d81a611670a0df6b96f9ba7
Ashley Hood <ashley.a.d.hood@gmail.com>
2017-07-10 15:04:19 +0200

    Pseudo Code is in the makeSlater now
    Title says it all.

--- Project/MakeSlater.py
+++ Project/MakeSlater.py
@@ -1,2 +1,12 @@
 import numpy
 
+def odoCounter(prevState):
+	if (len(states) < nParticles):
+		for i in range(prevState+1, nParticles):
+			state.append(i)		#appending the occupied SP state to the SD
+			odoCounter(i)		#the self-recursive function call
+
+
+
+def odoCounter(prevState):
+	
